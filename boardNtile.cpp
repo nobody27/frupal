4a6c7988064d931309a109f8b67f42bd7ffe63df
Leior Varon <leior@pdx.edu>
2019-04-13 21:57:26 -0700

    code review and refactor for boardNtile.*
    I cleaned up some extra tabs, I don't know if it was just in my
    viewer or everywhere but it appears as though there were multiple tabs
    for each indentation
    
    I also added a destructor for the board which is needed since the board
    is allocated on the heap
    
    I also unremarked some ifdefs I added beforehand to protect against
    multiple includes of the same header file (though I doubt it's needed in
    this case)
    
    One thing I did that may require a discussion is to shorten the text in
    each tiles display. I went under the assumption that less is more... :)

--- boardNtile.cpp
+++ boardNtile.cpp
@@ -12,19 +12,18 @@ using namespace std;
 
 Tile::Tile(int x, int y)
 {
-
-		xValue=x;
-		yValue=y;
-		terrain=0; //grassy meadow (later enum)
-		treasure=1; //no treasure - later string with description
+	xValue=x;
+	yValue=y;
+	terrain=0; //grassy meadow (later enum)
+	treasure=1; //no treasure - later string with description
 }
 
 void Tile::tileDisplay()
 {
-	cout << "		The tile's x value is: " << xValue<<endl;
-	cout << "		The tile's y value is: " << yValue<<endl;
-	cout << "		The tile's terrain is: " << terrain<<endl;
-	cout << "		The tile's treasure is: " << treasure<<endl;
+	cout << "	Position: (" << xValue << "," <<
+				yValue << ")" << endl;
+	cout << "	Terrain: " << terrain<<endl;
+	cout << "	Treasure: " << treasure<<endl;
 }
 
 
@@ -38,31 +37,35 @@ Board::Board()
 
 Board::Board(int size)
 {
-				boardSize = size;
-				boardArray = new Tile**[size];	
-				for(int i=0; i<size; ++i)
-				{
-								boardArray[i]= new Tile*[size];
-								for(int j=0; j<size; ++j)
-								{
-												boardArray[i][j]=new Tile(i,j);
-								}
-				}
+	boardSize = size;
+	boardArray = new Tile**[size];	
+	for(int i=0; i<size; ++i)
+	{
+		boardArray[i]= new Tile*[size];
+		for(int j=0; j<size; ++j)
+		{
+			boardArray[i][j]=new Tile(i,j);
+		}
+	}
 }
 
-void Board::display()
+Board::~Board()
 {
-				cout << "Displaying your island: " <<endl;
-				
-				for(int i=0; i<boardSize; ++i)
-				{
-								for(int j=0; j<boardSize; ++j)
-								{
-												cout <<"the Tile at (" << i << ", " << j <<") has  data" <<endl;
-												boardArray[i][j]->tileDisplay();
-								}
-								cout <<endl;
-				}
+	delete[] boardArray;
+}
+
+void Board::display() const
+{
+	cout << "Displaying your island: " <<endl;
+	for(int i=0; i<boardSize; ++i)
+	{
+		for(int j=0; j<boardSize; ++j)
+		{
+			cout <<"the Tile at (" << i << ", " << j <<") has  data" <<endl;
+			boardArray[i][j]->tileDisplay();
+		}
+		cout <<endl;
+	}
 }
 
 
--- boardNtile.h
+++ boardNtile.h
@@ -5,8 +5,8 @@
 //April 10, 2019
 //boardNtile.h
 
-//#ifndef BOARD_N_TILE_H
-//#define BOARD_N_TILE_H
+#ifndef BOARD_N_TILE_H
+#define BOARD_N_TILE_H
 
 //using namespace std;
 #include <iostream>
@@ -34,11 +34,12 @@ class Board
 	public:
 		//Board(); //constructor - this default creates a 10x10 board. Calls Board(10)
 		Board(int size=10); //constructor - argument from user sizeBoardxsizeBoard
-		//~Board();
-		void display();
+		//destructor is needed because we allocate the array of tiles
+		~Board();
+		void display() const;
 	
 	private:
 		int boardSize;
-		Tile***boardArray;
+		Tile*** boardArray;
 };
-//#endif //BOARD_N_TILE_H
+#endif //BOARD_N_TILE_H
