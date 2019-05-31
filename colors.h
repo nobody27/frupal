823e0673377ba81db648f476b4d6fb31e0dc1472
Max Ovitsky <bmo@pdx.edu>
2019-05-30 22:45:09 -0700

    added colors.h

---
+++ colors.h
@@ -0,0 +1,19 @@
+#ifndef COLORS
+#define COLORS
+
+#define RESET "\033[0m"
+#define BOLDRED "\033[31;1m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[34m"
+#define MAGENTA "\033[35m"
+#define BOLDMAGENTA "\033[1;35m"
+#define CYAN "\033[36m"
+#define GRAY "\033[90m"
+#define LIGHT_GREEN "\033[92m"
+#define LIGHT_YELLOW "\033[93m"
+#define CYANonBLUE "\033[36;44m"
+#define OBSTACLE_COLOR "\033[30;101m"
+
+#endif
