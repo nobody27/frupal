763f503453efdfa597e21182852f445fa7292ece
Max Ovitsky <bmo@pdx.edu>
2019-06-04 14:38:35 -0700

    fixed file writing bug

--- GameManager.cpp
+++ GameManager.cpp
@@ -221,7 +221,7 @@ void GameManager::readConfigFile(string config){
 			singleUse = stoi(value);
 			getline(configFile, value, ',');
 			price = stoi(value);
-			getline(configFile, value);
+			getline(configFile, value, '\n');
 			quantity = stoi(value);
 			//after getting the values, create a tool and push it to the vector	
 			Tool newTool(toolName, obstacleName, energySaved, singleUse, price, quantity);
@@ -274,6 +274,7 @@ void GameManager::writeConfigFile(){
 		configFile << TOOL_CONFIGURATION << "," 
 			<< it->name << ","
 			<< it->relevantObstacle << ","
+			<< it->singleUse << ","
 			<< it->energySaved << ","
 			<< it->price << ","
 			<< it->quantity << "\n";
